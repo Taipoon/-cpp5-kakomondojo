c41c1876b635fa663b77a2f86d0a35df58181970
平山 泰暉　hirayama taiki <taiki.hirayama.kpu@gmail.com>
2022-07-30 21:55:59 +0900

    SampleAnswerSerializerを作成、htmlを編集

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
-
 from kakomon.models import Course, Issue, SampleAnswer
+from rest_framework import serializers
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -12,17 +11,22 @@ class CourseSerializer(serializers.ModelSerializer):
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        # fields = '__all__'
         fields = ('id', 'title', 'content', 'lecture_number', )
 
 
 class SampleAnswerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SampleAnswer
         # Serializersに紐づけるmodelを定義する
+        model = SampleAnswer
 
         fields = '__all__'
 
-        extra_kwargs = {
+        extra_kwargs = {}
 
-        }
+    def create(self, validated_data):
+        sample_answer = SampleAnswer.objects.create(
+            text=validated_data['text'],
+            image=validated_data['image'],
+            isssue_id=validated_data['issue_id'],
+        )
+        return sample_answer
